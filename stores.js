aad8f1b965d4bfc0a048127497a25c87d639d8d1
douglasboliver <douglasbradfordoliver@gmail.com>
2020-02-16 00:00:21 -0500

    Commiting restaurants list

---
+++ stores.js
@@ -0,0 +1,79 @@
+const stores = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.395130,
+                37.786390
+            ]
+        },
+        "properties": {
+            "address": "555 Folsom St, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "DragonEats"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.399048,
+                37.785461
+            ]
+        },
+        "properties": {
+            "address": "22 Hawthorne St, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Benu"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.395142,
+                37.788528
+            ]
+        },
+        "properties": {
+            "address": "405 Howard St #310, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Spice Kit"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.397590,
+                37.789490
+            ]
+        },
+        "properties": {
+            "address": "100 First Plaza, 100 1st St #110, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Oasis Grill"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.397590,
+                37.789490
+            ]
+        },
+        "properties": {
+            "address": "255 Kearny St, San Francisco, CA 94108",
+            "city": "San Francisco",
+            "name": "SF Wraps"
+        }
+    }
+    ]
+};
